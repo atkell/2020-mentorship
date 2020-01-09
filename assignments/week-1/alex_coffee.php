16e05bf7f8c5ecf7221f69f25ce31fc6a820370f
akell <36044882+atkell@users.noreply.github.com>
2020-01-09 13:02:15 -0500

    Added a function to validate user input as well as to print the receipt

--- assignments/week-1/alex_coffee.php
+++ assignments/week-1/alex_coffee.php
@@ -1,20 +1,61 @@
 <?php
 
+//global $salesTax = 1.07;
+
 echo "Let's calculate the cost of your coffee purchase!\n";
+
+function inputValidation ($input)
+{
+    if ($input > 0.0) {
+//        echo "Valid input!";
+    } else {
+        echo "Invalid input! Let\'s try again, shall we? ";
+        getInputs();
+    }
+}
+
+# input validation:
+# what makes it valid?
+# 1) integer => is_numeric — Finds whether a variable is a number or a numeric string. Tip: this will also handle empty
+# 2) greater than 0.0 => $input > 0
+# 3) not empty => $input != ""
+# if input is not valid, let the user know and allow them to try again. we should make this
+# a function too...
+
+//if ($input != "" || $input > 0 || is_numeric($input) ) {
+//    echo "Error";
+//} else {
+//    proceed to next step
+//}
+
+# Prompting for input in this way will result in a string. We need to cast the input as an integer. If the input is
+# non-numeric or a mix of numeric and alphanumeric, the var_dump() will result in a int == 0, which is great!
+# There's a bug though, that if the number isn't a whole number (like an integer), it wont work. So let's try floatval()
+# instead...
+
 echo "Tell me how many pounds of coffee you want to buy? ";
-$weight = fgets(STDIN);
+$weight = floatval(fgets(STDIN));
+inputValidation($weight);
 
 echo "\nTell me the cost per pound of this coffee? ";
-$unitCost = fgets(STDIN);
+$unitCost = floatval(fgets(STDIN));
+inputValidation($unitCost);
 
 $salesTax = 1.07;
-
 $subTotal = $weight * $unitCost;
+$grandTotal = $subTotal * $salesTax;
+printReceipt($weight, $unitCost, $subTotal, $grandTotal);
 
-$totalCost = $subTotal * $salesTax;
-
-echo "----\n";
-echo "Sub-total:               $" . round($subTotal, 2) . "\n";
-echo "Grand total (w/ tax):    $" . round($totalCost, 2) . "\n";
-echo "Sales tax:               7.0%\n";
-echo "----\n";
+function printReceipt($weight, $unitCost, $subTotal, $grandTotal)
+{
+    echo "\n\n----\n";
+    echo "Thank you for shopping with us!";
+    echo "\n----\n";
+    echo "Weight (lbs.):           " . $weight . "\n";
+    echo "Unit Cost (per lbs.):    " . $unitCost;
+    echo "\n----\n";
+    echo "Sub-total:               $" . round($subTotal, 2) . "\n";
+    echo "Grand total (w/ tax):    $" . round($grandTotal, 2) . "\n";
+    echo "Sales tax:               7.0%\n";
+    echo "----\n";
+}
